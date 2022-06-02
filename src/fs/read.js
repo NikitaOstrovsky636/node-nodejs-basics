a4133126449abfbd62caa65f763556457e1a17c8
Nikita Ostrovsky <misha.ostrovsky1234567@gmail.com>
2022-06-02 21:28:07 +0300

    feat: add solution of read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,26 @@
+import { readFile, access } from 'node:fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';  
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+const fileExists = async path => access(path).then(() => true).catch(() => false);
+
 export const read = async () => {
-    // Write your code here 
+    try {
+        const isFileExists = await fileExists(filePath);
+        if (!isFileExists) {
+            throw new Error('FS operation failed');
+        } else {
+            let data = await readFile(filePath);
+            data = data.toString();
+            console.log(data);
+        }
+    } catch (err) {
+        throw new Error(err);
+    }
 };
+
+read();
