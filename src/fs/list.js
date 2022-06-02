5eb1403ff96ca1a7fcff896a7329385e1c5ffc4a
Nikita Ostrovsky <misha.ostrovsky1234567@gmail.com>
2022-06-02 21:08:43 +0300

    feat: add solution of list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,26 @@
+import { readdir, access } from 'node:fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';  
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const folderPath = path.join(__dirname, 'files');
+
+const folderExists = async path => access(path).then(() => true).catch(() => false);
+
 export const list = async () => {
-    // Write your code here 
+    try {
+        const isFolderExists = await folderExists(folderPath);
+
+        if (!isFolderExists) {
+            throw new Error('FS operation failed');
+        } else {
+            const files = await readdir(folderPath);
+            console.log(files);
+        }
+    } catch (err) {
+        throw new Error(err);
+    }
 };
+
+list();
