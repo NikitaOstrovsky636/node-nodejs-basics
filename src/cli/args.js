6f3601dfe014765bf71184303252c78563a1c3cc
Nikita Ostrovsky <misha.ostrovsky1234567@gmail.com>
2022-06-03 00:22:08 +0300

    feat: add solution of args.js task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,15 @@
+import {argv} from 'node:process';
+
 export const parseArgs = () => {
-    // Write your code here 
+    const arrOfArgs = [];
+
+    for (let i = 2; i < argv.length; i++) {
+        if (argv[i].startsWith('--')) {
+            arrOfArgs.push(`${argv[i].slice(2)} is ${argv[i + 1]}`);
+        }
+    }
+
+    console.log(arrOfArgs.join(', '));
 };
+
+parseArgs();
