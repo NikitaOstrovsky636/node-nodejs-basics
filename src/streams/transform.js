75512a882f412a50349b0998fa881020b305a7e2
Nikita Ostrovsky <misha.ostrovsky1234567@gmail.com>
2022-06-03 22:15:28 +0300

    feat: add solution of transform.js task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,19 @@
+import { captureRejections } from 'events';
+import { stdin, stdout } from 'process';
+import { Transform } from 'stream';
+
+const reverse = new Transform({
+    transform(chank, encoding, callback) {
+        callback(null, chank
+            .toString()
+            .split('')
+            .reverse()
+            .join(''));
+    }
+});
+
 export const transform = async () => {
-    // Write your code here 
+    stdin.pipe(reverse).pipe(stdout);
 };
+
+transform();
