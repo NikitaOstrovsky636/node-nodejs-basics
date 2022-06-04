e024b0bcd4cd24799e47368e27e8d9375e41284c
Nikita Ostrovsky <misha.ostrovsky1234567@gmail.com>
2022-06-04 08:48:35 +0300

    feat: add solution of Worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,39 @@
+import { Worker } from 'worker_threads';
+import {fileURLToPath} from 'url';
+import path, { resolve } from 'path';
+import os from 'os';
+import { rejects } from 'assert';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workerPath = path.join(__dirname, 'worker.js');
+
 export const performCalculations = async () => {
-    // Write your code here
+    const coresNum = os.cpus().length;
+    let arr = [];
+
+    for (let i = 10; i < coresNum + 10; i++) {
+
+        arr.push(
+            new Promise( (resolve, reject) => {
+                    new Worker(workerPath, {
+                        workerData: i
+                    }).on('message', data => {
+                        resolve({
+                            status: 'resolved',
+                            data: data
+                        });
+                    }).on('error', () => {
+                        reject({
+                            status: 'error',
+                            data: 'null'
+                        })
+                    })
+            })
+        )
+    }
+
+    return await Promise.all(arr);
 };
+
+console.log(await performCalculations());
