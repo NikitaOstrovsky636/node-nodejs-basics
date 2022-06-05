c5a01bc78056c81d73af342fce94e55ed90e5312
Nikita Ostrovsky <misha.ostrovsky1234567@gmail.com>
2022-06-05 23:29:18 +0300

    feat: add scripts into package.json

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,14 +1,15 @@
 import { spawn } from 'child_process';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { stdout } from 'process';
 
-export const __filename = fileURLToPath(import.meta.url);
-export const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const pathToFile = path.join(__dirname, 'files', 'script.js');
 
 export const spawnChildProcess = async (args) => {
-    const child = spawn(process.execPath, [pathToFile, ...args]);
+    const child = spawn('node', [pathToFile, ...args]);
     process.stdin.pipe(child.stdin);
     child.stdout.pipe(process.stdout);
 };
